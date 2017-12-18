57b4254247b5f5bcc7e79e4fc22805bc470c2ac5
wuanlan97 <wuanlan97@qq.com>
2017-12-19 03:30:20 +0800

    完成日期选择控件

---
+++ app/src/main/java/com/example/wuanlan97/androidexam/setDatePicker.java
@@ -0,0 +1,13 @@
+package com.example.wuanlan97.androidexam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class setDatePicker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_date_picker);
+    }
+}
